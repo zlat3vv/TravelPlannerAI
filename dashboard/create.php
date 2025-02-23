c688cba266cd5cc3f91ec7649edd0712b3923740
zlat3vv <bgfighter615@gmail.com>
2025-02-23 12:58:21 +0200

    Fixed bugs

--- dashboard/create.php
+++ dashboard/create.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: ../auth/register.php?error=Please+login+first");
+    exit();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
